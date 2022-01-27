f027a83ebd288f45dcf368165518e49dce7127b7
Oleksandr Porunov <alex@mapped.com>
2022-01-27 15:57:32 +0200

    Add simple AstraDB write test
    Signed-off-by: Oleksandr Porunov <alex@mapped.com>

--- src/main/java/com/mapped/astra/Main.java
+++ src/main/java/com/mapped/astra/Main.java
@@ -1,6 +1,7 @@
 package com.mapped.astra;
 
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.janusgraph.core.JanusGraph;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 
 @SpringBootApplication(scanBasePackages = "com.mapped.astra")
 public class Main {
@@ -27,11 +29,44 @@ public class Main {
 
         LOGGER.info("Application is started");
 
-        GraphTraversalSource traversalSource = janusGraph.traversal();
+        long vertexId = System.currentTimeMillis();
 
-        traversalSource.V().hasNext();
+        ArrayList<Long> timesCollection = new ArrayList<>();
 
-        LOGGER.info("The test case is finished");
+        for(int k = 0; k<20; k++){
+            long start = System.currentTimeMillis();
+
+            for(int j=0; j<10; j++){
+                GraphTraversalSource traversalSource = janusGraph.traversal();
+
+                for(int i=0; i<1000; i++){
+                    ++vertexId;
+                    Vertex vertex1 = traversalSource.addV().property("graphId", "ASTRA_TEST")
+                            .property("id", "id_"+vertexId)
+                            .property("name", "Vertex name "+vertexId).next();
+                    Vertex vertex2 = traversalSource.addV().property("graphId", "ASTRA_TEST")
+                            .property("id", "id_"+vertexId)
+                            .property("name", "Vertex name "+vertexId).next();
+                    vertex1.addEdge("hasPart", vertex2);
+                }
+
+                janusGraph.tx().commit();
+            }
+
+            long timeTook = System.currentTimeMillis()-start;
+
+            LOGGER.info("Write took: "+timeTook+" ms");
+
+            timesCollection.add(timeTook);
+        }
+
+        long sum = 0;
+
+        for(Long timeTook : timesCollection){
+            sum+=timeTook;
+        }
+
+        LOGGER.info("The test case is finished. Average execution time for a test is: "+(sum/timesCollection.size())+" ms; All tests took: "+sum+" ms.");
 
     }
 
